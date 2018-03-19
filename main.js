4deae93120765aedcabf916b7ef2670ddca29d74
dylan? <dms745@nyu.edu>
2018-03-19 14:21:42 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
+
+
+
 function score() {
 
 	var question1 = document.quiz.question1.value;
 	var question2 = document.quiz.question2.value;
 	var question3 = document.quiz.question3.value;
 	var question4 = document.quiz.question4.value;
+	var question5 = document.quiz.question5.value;
+	var question6 = document.quiz.question6.value;
+	var question7 = document.quiz.question7.value;
+	var question8 = document.quiz.question8.value;
 
 	var correct = 0;
 
@@ -24,6 +31,30 @@ function score() {
 	}
 
 	if (question4 == "Wow") {
+		correct++;
+	}
+
+	if (question5 == "yes") {
+		correct++;
+	}
+
+	if (question5 == "yeah") {
+		correct++;
+	}
+
+	if (question5 == "infinity") {
+		correct++;
+	}
+
+	if (question6 == "peru") {
+		correct++;
+	}
+
+	if (question7 == "turkey") {
+		correct++;
+	}
+
+	if (question8 == "villain") {
 		correct++;
 	}
 
